a274ff4e2edc4bc182331f15eaf105e40ccb928b
Park Juhyun <wngus6429@gmail.com>
2024-10-11 02:50:36 +0900

    Mui, Grid 수정

--- front/src/app/(beforeLogin)/signup/page.tsx
+++ front/src/app/(beforeLogin)/signup/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import {
   Avatar,
   Button,
@@ -8,24 +8,24 @@ import {
   FormControl,
   FormControlLabel,
   Checkbox,
-  Grid,
   Box,
   Typography,
   Container,
 } from "@mui/material/";
+import Grid from "@mui/material/Grid2";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const Register = () => {
+const Register = (): ReactNode => {
   const theme = createTheme();
   const [, setChecked] = useState(false);
 
   // 동의 체크
-  const handleAgree = (event) => {
-    setChecked(event.target.checked);
+  const handleAgree = (e: any): void => {
+    setChecked(e.target.checked);
   };
 
   // form 전송
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: any): void => {
     e.preventDefault();
   };
 
@@ -45,26 +45,13 @@ const Register = () => {
           <Typography component="h1" variant="h5">
             회원가입
           </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
-          >
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <FormControl component="fieldset" variant="standard">
               <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    autoFocus
-                    fullWidth
-                    type="email"
-                    id="email"
-                    name="email"
-                    label="이메일 주소"
-                  />
+                <Grid size={12}>
+                  <TextField required autoFocus fullWidth type="email" id="email" name="email" label="이메일 주소" />
                 </Grid>
-                <Grid item xs={12}>
+                <Grid size={12}>
                   <TextField
                     required
                     fullWidth
@@ -74,7 +61,7 @@ const Register = () => {
                     label="비밀번호 (숫자+영문자+특수문자 8자리 이상)"
                   />
                 </Grid>
-                <Grid item xs={12}>
+                <Grid size={12}>
                   <TextField
                     required
                     fullWidth
@@ -84,31 +71,17 @@ const Register = () => {
                     label="비밀번호 재입력"
                   />
                 </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="name"
-                    name="name"
-                    label="이름"
-                  />
+                <Grid size={12}>
+                  <TextField required fullWidth id="name" name="name" label="이름" />
                 </Grid>
-                <Grid item xs={12}>
+                <Grid size={12}>
                   <FormControlLabel
-                    control={
-                      <Checkbox onChange={handleAgree} color="primary" />
-                    }
+                    control={<Checkbox onChange={handleAgree} color="primary" />}
                     label="회원가입 약관에 동의합니다."
                   />
                 </Grid>
               </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                size="large"
-              >
+              <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} size="large">
                 회원가입
               </Button>
             </FormControl>

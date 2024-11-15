f063018815fc08516a25c0dfb06111b1878dff86
ParkJuhyun <wngus6429@gmail.com>
2024-11-15 21:39:33 +0900

    상세페이지 개선

--- front/src/app/(afterLogin)/detail/[id]/page.tsx
+++ front/src/app/(afterLogin)/detail/[id]/page.tsx
@@ -2,9 +2,10 @@
 import Loading from "@/app/components/common/Loading";
 import { useMessage } from "@/app/store";
 import { ImageType, StoryType } from "@/app/types/types";
-import { Button, Select } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import dayjs from "dayjs";
 import { useParams, useRouter } from "next/navigation";
 import { ReactNode, useEffect, useState } from "react";
 
@@ -61,52 +62,77 @@ export default function page(): ReactNode {
 
   // TODO : comments 테이블 만들어서 엮기
   return (
-    <>
+    <Box display="flex" justifyContent="center" alignItems="center" padding={2}>
       {detail && (
-        <>
-          <div>{detail.title}</div>
-          <div>{detail.content}</div>
-          <div>{detail.nickname}</div>
-          <div>{detail.created_at}</div>
-          <div>{detail.read_count}</div>
-          <Button
-            sx={{ padding: "0px" }}
-            size="small"
-            variant="outlined"
-            onClick={(e) => {
-              e.preventDefault();
-              deleteData.mutate(detail.id);
-            }}
-            color="warning"
-          >
-            수정하기
-          </Button>
-          <Button
-            sx={{ padding: "0px" }}
-            size="small"
-            variant="outlined"
-            onClick={(e) => {
-              e.preventDefault();
-              console.log("detail.id", detail.id);
-              deleteData.mutate(detail.id);
-            }}
-            color="error"
-          >
-            삭제
-          </Button>
+        <Card sx={{ width: "100%", boxShadow: 3, padding: 2 }}>
+          <CardContent>
+            <Typography
+              variant="h4"
+              component="div"
+              gutterBottom
+              sx={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <div>{detail.title}</div>
+              <div>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="warning"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    deleteData.mutate(detail.id);
+                  }}
+                >
+                  수정하기
+                </Button>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="error"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    deleteData.mutate(detail.id);
+                  }}
+                >
+                  삭제
+                </Button>
+              </div>
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              gutterBottom
+              sx={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <div>작성자: {detail.nickname}</div>
+              <div>
+                작성일: {dayjs(detail.created_at).format("YYYY/MM/DD HH:mm:ss")}
+                <div>조회수: {detail.read_count}</div>
+              </div>
+            </Typography>
+            <Typography variant="body1" color="text.secondary" gutterBottom>
+              {detail.content}
+            </Typography>
+          </CardContent>
           {detail.Image && detail.Image.length > 0 && (
-            <>
-              <h2>첨부된 이미지:</h2>
-              {/* src에 localhost:백엔드 안 적는 이유는 proxy 설정해서 그럼. next.config.js */}
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                첨부된 이미지:
+              </Typography>
               {detail.Image.map((img: ImageType, index: number) => (
-                <div key={`${img.imageId}-${index}`}>
-                  <img src={img.link} alt={`첨부 이미지 ${index + 1}`} style={{ maxWidth: "100%", height: "auto" }} />
-                </div>
+                <CardMedia
+                  /* src에 localhost:백엔드 안 적는 이유는 proxy 설정해서 그럼. next.config.js */
+                  key={`${img.imageId}-${index}`}
+                  component="img"
+                  image={img.link}
+                  alt={`첨부 이미지 ${index + 1}`}
+                  sx={{ marginY: 1, borderRadius: 1 }}
+                />
               ))}
-            </>
+            </Box>
           )}
-        </>
+        </Card>
       )}
-    </>
+    </Box>
   );
 }

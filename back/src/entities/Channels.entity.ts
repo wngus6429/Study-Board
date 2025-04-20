5c9e94983973055b8d157f9516148241959411a3
ParkJuhyun <wngus6429@gmail.com>
2025-04-20 17:36:57 +0900

    채널 테이블 첫 만듬

---
+++ back/src/entities/Channels.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() // 이 클래스가 데이터베이스의 엔티티임을 선언합니다.
+export class Channels {
+  @PrimaryGeneratedColumn() // 자동 증가하는 기본 키 컬럼
+  id: number;
+
+  @Column() // 채널 이름
+  ChannelName: string;
+
+  @Column() // 게시글 갯수 카운트 (페이지네이션을 위해)
+  StoryCount: number;
+}

07ec35febfd75ba7e86fe31cae9097cd6a3aaead
Park Juhyun <wngus6429@gmail.com>
2025-06-15 04:39:23 +0900

    Update VIEW_COUNT.ts

--- front/src/app/const/VIEW_COUNT.ts
+++ front/src/app/const/VIEW_COUNT.ts
@@ -1,7 +1,7 @@
-export const TABLE_VIEW_COUNT: number = 5; // 일반 데이터 테이블 페이지 당 몇개 보일지
-export const USER_TABLE_VIEW_COUNT: number = 5; // 유저 프로필, 페이지 당 몇개 보일지
+export const TABLE_VIEW_COUNT: number = 6; // 일반 데이터 테이블 페이지 당 몇개 보일지
+export const USER_TABLE_VIEW_COUNT: number = 6; // 유저 프로필, 페이지 당 몇개 보일지
 export const MIN_RECOMMEND_COUNT: number = 1; // 추천 갯수 몇개 이상, 추천랭킹에 뜰지 정함
-export const COMMENT_VIEW_COUNT: number = 10; // 댓글 페이지 당 몇개 보일지
-export const NOTIFICATION_VIEW_COUNT: number = 7; // 알림 페이지 당 몇개 보일지
+export const COMMENT_VIEW_COUNT: number = 6; // 댓글 페이지 당 몇개 보일지
+export const NOTIFICATION_VIEW_COUNT: number = 6; // 알림 페이지 당 몇개 보일지
 // 채널 관련 설정 상수들
 export const CHANNEL_TOP_STORIES_COUNT = 5; // 채널별 개념글 표시 개수

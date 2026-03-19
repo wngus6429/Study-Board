dde372dfebf6532c7ada1fecd768c86f7be6ecf1
ParkJuhyun <wngus6429@gmail.com>
2026-03-19 20:47:13 +0900

    비밀번호 대충

--- front/src/app/(beforeLogin)/signup/page.tsx
+++ front/src/app/(beforeLogin)/signup/page.tsx
@@ -341,7 +341,7 @@ const SignupPage = (): ReactNode => {
                     fullWidth
                     type="password"
                     id="password"
-                    label="비밀번호 (숫자+영문자+특수문자 8자리 이상)"
+                    label="비밀번호 (영문+숫자 4자리 이상)"
                     {...register("password")}
                     error={!!errors.password}
                     helperText={errors.password?.message}
